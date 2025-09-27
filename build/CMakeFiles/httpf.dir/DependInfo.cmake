6327da9c504b8e0842398b5bddf00d3c8be4d051
“wzhhh3” <1979196768@qq.com>
2025-09-27 19:58:35 +0800

    完成buffer

---
+++ build/CMakeFiles/httpf.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/webserver/src/buffer.cpp" "/webserver/build/CMakeFiles/httpf.dir/webserver/src/buffer.cpp.o"
+  "/webserver/src/main.cpp" "/webserver/build/CMakeFiles/httpf.dir/webserver/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/httpf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/httpf.dir/webserver/src/main.cpp.o"
+  "CMakeFiles/httpf.dir/webserver/src/buffer.cpp.o"
+  "/webserver/bin/httpf.pdb"
+  "/webserver/bin/httpf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/httpf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
